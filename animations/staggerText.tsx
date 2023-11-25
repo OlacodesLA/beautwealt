0273ee6afea1da09f78907ca7f8247e13a55f9f6
OlacodesLA <zaccheausjide@gmail.com>
2023-11-25 08:39:53 +0100

    :lipstick: Style: staggertext fix

--- animations/staggerText.tsx
+++ animations/staggerText.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { motion, useInView, useAnimation, Variant } from "framer-motion";
 import { useEffect, useRef } from "react";
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
 "use client";
+import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { Facebook, Instagram, Twitter } from "lucide-react";
 import StaggerChildren, { childVariants } from "@/animations/staggerChildren";
-import { AnimatePresence, motion } from "framer-motion";
-import { StaggerText } from "@/animations/staggerText";
-import { useEffect, useState } from "react";
+import {
+  AnimatePresence,
+  motion,
+  useInView,
+  useAnimation,
+  Variant,
+} from "framer-motion";
 
 export default function Home() {
   const [pulse, setPulse] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setPulse(true);
-    }, 7000);
-  }, []);
-
   const socials = [
     {
       id: 1,
@@ -32,6 +30,12 @@ export default function Home() {
       href: "https://www.instagram.com/beautwealt_electronics/",
     },
   ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPulse(true);
+    }, 7000);
+  }, []);
 
   return (
     <AnimatePresence mode="wait">
@@ -60,19 +64,19 @@ export default function Home() {
               <StaggerText
                 once
                 text="Coming soon..."
-                el="p"
+                el="div"
                 className="italic font-bold text-white mt-[1.8px]"
               />
             </div>
           </motion.div>
 
-          <ul className="mt-2 flex justify-center w-full items-center space-x-2">
+          <ul className="mt-2 flex justify-center w-full items-center space-x-4">
             {socials.map((social, i) => (
               <motion.li
                 key={i}
                 initial={{
                   opacity: 0,
-                  translateY: 100,
+                  translateY: 70,
                 }}
                 animate={{
                   opacity: 1,
@@ -95,3 +99,99 @@ export default function Home() {
     </AnimatePresence>
   );
 }
+
+type StaggerTextProps = {
+  text: string | string[];
+  el?: keyof JSX.IntrinsicElements;
+  className?: string;
+  once?: boolean;
+  repeatDelay?: number;
+  animation?: {
+    hidden: Variant;
+    visible: Variant;
+  };
+};
+
+const defaultAnimations = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.1,
+    },
+  },
+};
+
+const StaggerText = ({
+  text,
+  el: Wrapper = "p",
+  className,
+  once,
+  repeatDelay,
+  animation = defaultAnimations,
+}: StaggerTextProps) => {
+  const controls = useAnimation();
+  const textArray = Array.isArray(text) ? text : [text];
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 0.5, once });
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+    const show = () => {
+      controls.start("visible");
+      if (repeatDelay) {
+        timeout = setTimeout(async () => {
+          await controls.start("hidden");
+          controls.start("visible");
+        }, repeatDelay);
+      }
+    };
+
+    if (isInView) {
+      show();
+    } else {
+      controls.start("hidden");
+    }
+
+    return () => clearTimeout(timeout);
+  }, [isInView]);
+
+  return (
+    <Wrapper className={className}>
+      <span className="sr-only">{textArray.join(" ")}</span>
+      <motion.span
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={{
+          visible: { transition: { staggerChildren: 0.1 } },
+          hidden: {},
+        }}
+        aria-hidden
+      >
+        {textArray.map((line, lineIndex) => (
+          <span className="block" key={`${line}-${lineIndex}`}>
+            {line.split(" ").map((word, wordIndex) => (
+              <span className="inline-block" key={`${word}-${wordIndex}`}>
+                {word.split("").map((char, charIndex) => (
+                  <motion.span
+                    key={`${char}-${charIndex}`}
+                    className="inline-block"
+                    variants={animation}
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+                <span className="inline-block">&nbsp;</span>
+              </span>
+            ))}
+          </span>
+        ))}
+      </motion.span>
+    </Wrapper>
+  );
+};
